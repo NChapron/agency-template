4f5299e4b625db9a8266f481dcb0a06c08a7f460
Nicolas <nicolas.chapron@uleis.co.uk>
2022-10-11 12:20:36 +0100

    Added title left possibility

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -4,14 +4,17 @@ import classes from "./Title.module.css";
 import dot from "../../assets/title/ellipsis.png";
 
 interface TitleProps {
+  align?: string;
   children: React.ReactNode;
 }
 
 const Title: React.FC<TitleProps> = (props) => {
-  const { children } = props;
+  const { align, children } = props;
+
+  const dataAlign = !align ? "center" : align;
 
   return (
-    <div className={classes.title}>
+    <div className={classes.title} data-align={dataAlign}>
       <h2>{children}</h2>
       <span>
         <img src={dot} />
